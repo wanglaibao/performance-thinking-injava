e11705c0ac229e6b9cfbd645d22ebc4a5d16ff31
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-08-21 15:41:41 +0800

    add test java file info

--- springakka/src/main/java/com/laibao/springakka/actor/GreetingActor.java
+++ springakka/src/main/java/com/laibao/springakka/actor/GreetingActor.java
@@ -2,6 +2,8 @@ package com.laibao.springakka.actor;
 
 import akka.actor.AbstractActor;
 import akka.actor.Status;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import com.laibao.springakka.model.Greet;
 import com.laibao.springakka.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,17 @@ import org.springframework.stereotype.Component;
  * @author laibao wang
  * @date 2018-08-19
  * @version 1.0
+ *  Also notice the Spring annotations @Component and @Scope
+ *  these define the class as a Spring-managed bean with the prototype scope.
+ *  The scope is very important, because every bean retrieval request should result in a newly created instance,
+ *  as this behavior matches Akka’s actor lifecycle.
+ *  If you implement this bean with some other scope,
+ *  the typical case of restarting actors in Akka will most likely function incorrectly.
  */
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class GreetingActor extends AbstractActor {
+    private final LoggingAdapter logger = Logging.getLogger(context().system(), this);
 
     @Autowired
     private GreetingService greetingService;
@@ -24,6 +33,8 @@ public class GreetingActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return receiveBuilder().match(Greet.class,greet -> sender().tell(greetingService.greet(greet.getName()),self()))
-                                .matchAny(x -> sender().tell(new Status.Failure(new Exception("unkown message")),self())).build();
+                                //.matchAny(x -> sender().tell(new Status.Failure(new Exception("unkown message")),self()))
+                                .matchAny(o -> logger.info("received unknown message: {}", o))
+                                .build();
     }
 }
--- springakka/src/test/java/com/laibao/springakka/SpringAkkaIntegrationTest.java
+++ springakka/src/test/java/com/laibao/springakka/SpringAkkaIntegrationTest.java
@@ -3,6 +3,7 @@ package com.laibao.springakka;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.util.Timeout;
+import com.alibaba.fastjson.JSON;
 import com.laibao.springakka.confiruration.AppConfiguration;
 import com.laibao.springakka.extension.SpringExtension;
 import com.laibao.springakka.model.Greet;
@@ -20,6 +21,7 @@ import scala.concurrent.duration.FiniteDuration;
 import java.util.concurrent.TimeUnit;
 
 import static akka.pattern.Patterns.ask;
+import static com.laibao.springakka.extension.SpringExtension.SPRING_EXTENSION_PROVIDER;
 
 
 /**
@@ -37,21 +39,17 @@ public class SpringAkkaIntegrationTest {
 
     @Test
     public void whenCallingGreetingActor_thenActorGreetsTheCaller() throws Exception {
-
-        //final ActorRef greeter = getContext().actorOf(Props.create(Greeter.class), "greeter");
-
-
-        ActorRef greeter = actorSystem.actorOf(SpringExtension.SPRING_EXTENSION_PROVIDER.get(actorSystem).props("greetingActor"));
-
-
-        FiniteDuration duration = FiniteDuration.create(1, TimeUnit.SECONDS);
+        ActorRef greeter = actorSystem.actorOf(SPRING_EXTENSION_PROVIDER.get(actorSystem).props("greetingActor"), "greeter");
+        FiniteDuration duration = FiniteDuration.create(5, TimeUnit.SECONDS);
         Timeout timeout = Timeout.durationToTimeout(duration);
         Future<Object> result = ask(greeter, new Greet("John"), timeout);
         Assert.assertEquals("Hello, John", Await.result(result, duration));
+        Await.result(result, duration);
+        System.out.println(JSON.toJSONString(Await.result(result, duration)));
     }
 
     @After
     public void tearDown() {
-        actorSystem.settings().JvmShutdownHooks();
+        actorSystem.terminate();
     }
 }
