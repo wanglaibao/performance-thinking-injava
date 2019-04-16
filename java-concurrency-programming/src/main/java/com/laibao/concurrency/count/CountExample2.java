215a91c3bc6414acdc8cd44b6ee3dc6cf05a84e0
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-17 00:37:12 +0800

    update java file info

--- java-concurrency-programming/src/main/java/com/laibao/concurrency/count/CountExample2.java
+++ java-concurrency-programming/src/main/java/com/laibao/concurrency/count/CountExample2.java
@@ -1,6 +1,5 @@
 package com.laibao.concurrency.count;
 
-
 import com.laibao.concurrency.annoations.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,17 +7,11 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
-/**
- * 对于下面的例子，我们可以看到，每次输出的结果都是小于5000，并且值不是固定的，
- * 也就是发生了所谓的线程安全问题，如果信号量semaphore 修改为1，
- * 则每次只产生一个线程，不会发生线程安全问题
- */
 @Slf4j
 @ThreadSafe
-public class CountExample1 {
-
+public class CountExample2 {
     // 请求总数
     public static int clientTotal = 5000;
 
@@ -26,7 +19,7 @@ public class CountExample1 {
     public static int threadTotal = 200;
 
 
-    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+    private static AtomicLong atomicLong = new AtomicLong(0);
 
     public static void main(String[] args) throws Exception {
         ExecutorService executorService = Executors.newCachedThreadPool();
@@ -36,7 +29,7 @@ public class CountExample1 {
             executorService.execute(() -> {
                 try {
                     semaphore.acquire();
-                    add();
+                    addLongValue();
                     semaphore.release();
                 } catch (Exception ex) {
                     log.error("exception", ex.getMessage());
@@ -44,12 +37,13 @@ public class CountExample1 {
                 countDownLatch.countDown();
             });
         }
+        System.out.println();
         countDownLatch.await();
         executorService.shutdown();
-        log.info("count:{}", atomicInteger.get());
+        log.info("count:{}", atomicLong.get());
     }
 
-    private static void add() {
-        atomicInteger.incrementAndGet();
+    private static void addLongValue() {
+        atomicLong.incrementAndGet();
     }
 }
